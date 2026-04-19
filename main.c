da89c16b3b1e96e9bcd84312cf87e305a64453ed
Nikolay Matskevich <kolyan_krytoj@mail.ru>
2026-04-19 15:26:02 +0700

    Added main 3 func for work with Mx (create, print, free)

--- main.c
+++ main.c
@@ -1,4 +1,62 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void freeMx(int** Mx, size_t rows)
+{
+    if (Mx)
+    {
+        for (size_t i = 0; i < rows; i++)
+        {
+            free(Mx[i]);
+        }
+    }
+    free(Mx);
+}
+
+int** CreateMx(size_t rows, size_t columns, int A, int B)
+{
+    int **dmas = (int **)calloc(rows, sizeof(int*));
+    if (dmas)
+    {
+        for (size_t i = 0; i < rows; i++)
+        {
+            dmas[i] = (int *)calloc(columns, sizeof(int));
+            for (size_t k = 0; k < columns; k++)
+            {
+                dmas[i][k] = A + rand() % (B - A + 1);
+            }
+        }
+        return dmas;
+    }
+    else
+    {
+        freeMx(dmas, rows);
+    }
+}
+
+void printMx(int** Mx, int rows, int columns)
+{
+    if (Mx)
+    {
+        for (int ix = 0; ix < rows; ix++)
+        {
+            if (Mx[ix])
+            {
+                for (int kx = 0; kx < columns; kx++)
+                {
+                    printf("%d ", Mx[ix][kx]);
+                }
+                printf("\n");
+            }
+            else
+            {
+                printf("Null");
+            }
+        }
+    }
+}
+
 
 int main()
 {
