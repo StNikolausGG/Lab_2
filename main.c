c3cc0278b21a26e583bb1840226d6bac0d091467
Nikolay Matskevich <kolyan_krytoj@mail.ru>
2026-05-19 09:56:56 +0700

    No changes in original Mx

--- main.c
+++ main.c
@@ -73,17 +73,21 @@ int** Transponate(int **Mx, size_t rows, size_t columns)
 
         if (TrspMx)
         {
-            for (size_t i = 0; i < rows; i++)
-            {
-                if (!Mx[i]) Mx[i] = (int*)calloc(columns, sizeof (int));
-            }
-
             for (size_t i = 0; i < columns; i++)
             {
                 TrspMx[i] = (int *)calloc(rows, sizeof (int));
                 for (size_t k = 0; k < rows; k++)
                 {
-                    TrspMx[i][k] = Mx[k][i];
+                    int val = 0;
+                    if (Mx[k] != NULL)
+                    {
+                        val = Mx[k][i];
+                    }
+                    else
+                    {
+                        val = 0;
+                    }
+                    TrspMx[i][k] = val;
                 }
             }
         }
@@ -98,12 +102,12 @@ int** Transponate(int **Mx, size_t rows, size_t columns)
 int main()
 {
     //Проверка функции freeMx
-    int rows = 3;
-    int cols = 3;
-    int** Mx = CreateMx(rows, cols, 1, 10);
-    freeMx(&Mx, rows);
-    printMx(Mx, rows, cols);
-    printf("\n");
+//    int rows = 3;
+//    int cols = 3;
+//    int** Mx = CreateMx(rows, cols, 1, 10);
+//    freeMx(&Mx, rows);
+//    printMx(Mx, rows, cols);
+//    printf("\n");
 
     //Проверка функций createMx и printMx
 //    int rows = 3;
@@ -142,22 +146,22 @@ int main()
 //    else printMx(newMx, cols, rows);
 
     //Транспонирование полностью разреженной матрицы
-//    int rows = 3;
-//    int cols = 4;
-//    int** Mx = CreateMx(rows, cols, 1, 10);
-//    free(Mx[0]);
-//    Mx[0] = NULL;
-//    free(Mx[1]);
-//    Mx[1] = NULL;
-//    free(Mx[0]);
-//    Mx[2] = NULL;
+    int rows = 3;
+    int cols = 4;
+    int** Mx = CreateMx(rows, cols, 1, 10);
+    free(Mx[0]);
+    Mx[0] = NULL;
+    free(Mx[1]);
+    Mx[1] = NULL;
+    free(Mx[0]);
+    Mx[2] = NULL;
 
-//    printMx(Mx, rows, cols);
-//    printf("\n");
+    printMx(Mx, rows, cols);
+    printf("\n");
 
-//    int** newMx = Transponate(Mx, rows, cols);
-//    if (!newMx) printf("Null adress recieved!");
-//    else printMx(newMx, cols, rows);
+    int** newMx = Transponate(Mx, rows, cols);
+    if (!newMx) printf("Null adress recieved!");
+    else printMx(newMx, cols, rows);
 
 //    Нулевые адреса
 //    int rows = 3;
